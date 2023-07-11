72082e517ead670098ae4527896243c54dd73ea7
Fajar Abdurrohman <48176273+fajar2510@users.noreply.github.com>
2023-07-11 12:45:57 +0000

    add-shop-firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,14 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from "../shop-data.json";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContex = createContext({
     products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-    // eslint-disable-next-line no-unused-vars
-    const [products, setProducts] = useState(PRODUCTS);
+    
+    const [products, setProducts] = useState([]);
+
+
     const value = { products };
     return (
         <ProductsContex.Provider value={value}>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,7 +10,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch } from "firebase/firestore";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
@@ -39,6 +39,25 @@ export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey, 
+  objectsToAdd,
+  field 
+  ) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  })
+
+  await batch.commit();
+  console.log("done adding documents")
+
+
+}
 
 export const createUserDocumentFromAuth = async (
   userAuth,
