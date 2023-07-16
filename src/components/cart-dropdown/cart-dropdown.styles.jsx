b4f0fbb0cf74d531f24dcd81f513e3ac489ca70a
Fajar Abdurrohman <48176273+fajar2510@users.noreply.github.com>
2023-07-16 03:11:31 +0000

    input-styling

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,14 +1,11 @@
-import styled from "styled-components"
-import {
-  BaseButton,
-  GoogleSignInButton,
-  InvertedButton,
-} from "../button/button.styles"
+import styled from 'styled-components';
+
+import {Button} from '../button/button.component';
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
   width: 240px;
-  height: 240px;
+  height: 340px;
   display: flex;
   flex-direction: column;
   padding: 20px;
@@ -18,22 +15,19 @@ export const CartDropdownContainer = styled.div`
   right: 40px;
   z-index: 5;
 
-  ${BaseButton}, 
-  ${GoogleSignInButton}, 
-  ${InvertedButton} {
+  ${Button} {
     margin-top: auto;
   }
-`
+`;
 
 export const EmptyMessage = styled.span`
   font-size: 18px;
   margin: 50px auto;
-`
+`;
 
 export const CartItems = styled.div`
   height: 240px;
   display: flex;
   flex-direction: column;
   overflow: scroll;
-`
-
+`;
